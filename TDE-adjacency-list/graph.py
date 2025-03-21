e37fc3d77c65e11fdc218be790f80730aa34ba58
Matheus Girardi <matheusmgirardi@gmail.com>
2025-03-20 22:25:28 -0300

    tarefa lista de adjacencia

---
+++ TDE-adjacency-list/graph.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+
+class Grafo():
+    def __init__(self):
+        self.adj_list = defaultdict(list)
+        pass
+
+    def adiciona_vertice(self, vertice):
+        self.adj_list = 
+
+    def remove_vertice(self, vertice):
+        pass
+
+    def adiciona_aresta(self, vertice1, vertice2, peso):
+        pass
+
+    def remove_aresta(self, vertice1, vertice2):
+        pass
+
+    def tem_aresta(vertice1, vertice2):
+        pass
+
+    def grau_entrada(vertice):
+        pass
+
+    def grau_saida(vertice):
+        pass
+
+    def grau(vertice):
+        pass
---
+++ TDE-adjacency-list/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from tde-adjacency-list!")
+
+
+if __name__ == "__main__":
+    main()
